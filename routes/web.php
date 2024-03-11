10d3fb3b3e4c9c33c6afcbf79e4f00f4f032b14e
leonardoolia <leonardooliaa@gmail.com>
2024-03-11 16:45:50 +0100

    created routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MoviesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', HomeController::class)->name('home');
+Route::get('/movies', MoviesController::class)->name('movies');
+
